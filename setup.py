26ccd8b4bde5fe4d13fe9556864fcb7f90636706
ybrs <aybars.badur@gmail.com>
2014-12-09 13:15:09 +0200

    fixes pyuv version change

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='single-beat',
-    version='0.1.3',
+    version='0.1.4',
     long_description=__doc__,
     description='ensures only one instance of your process across your servers',
     url='https://github.com/ybrs/single-beat',
     packages=['singlebeat'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['pyuv', 'redis'],
+    install_requires=['pyuv==0.10.11',
+        'redis==2.9.1'],
     entry_points={
         'console_scripts': [
             'single-beat = singlebeat.beat:run_process',
